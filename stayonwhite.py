bcea5664a0a4786feb4483913912a16607b744e3
Jonas <fleischer.jm@gmx.de>
2017-05-28 00:59:36 +0200

    stay on white

--- stayonwhite.py
+++ stayonwhite.py
@@ -1,26 +1,6 @@
 #!/usr/bin/env python3
 from ev3dev.ev3 import *
 
-
-
-a.run_timed(time_sp=3000, speed_sp=500)
-b.run_timed(time_sp=3000, speed_sp=500)
-a.run_timed(time_sp=3000, speed_sp=-500)
-b.run_timed(time_sp=3000, speed_sp=-500)
-
-colors = {
-    0: 'No color',
-    1: 'Black',
-    2: 'Blue',
-    3: 'Green',
-    4: 'Yellow',
-    5: 'Red',
-    6: 'White',
-    7: 'Brown',
-}
-while True:
-    print(colors[ir.color])
-
 running = True
 a = LargeMotor('outA')
 b = LargeMotor('outB')
